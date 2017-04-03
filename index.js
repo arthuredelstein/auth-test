b62a9431f11851ab82efc3fbfaaa66892c491ade
Arthur Edelstein <arthuredelstein@gmail.com>
2017-04-03 12:40:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http')
+var auth = require('basic-auth')
+
+// Create server
+var server = http.createServer(function (req, res) {
+  var credentials = auth(req)
+
+  if (!credentials || credentials.name !== 'username' || credentials.pass !== 'password') {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+    res.end('Access denied')
+  } else {
+    res.end('Access granted')
+  }
+})
+
+// Listen
+server.listen(3002)
